624196267cec1529a7c699a553c09e3b9a2cc8cc
minggo <nudtzxm@gmail.com>
2019-12-04 14:34:23 +0800

    add iOS support

---
+++ src/ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-test
+//
+//  Created by minggo on 2019/12/4.
+//  Copyright © 2019 minggo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ src/ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-test
+//
+//  Created by minggo on 2019/12/4.
+//  Copyright © 2019 minggo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
