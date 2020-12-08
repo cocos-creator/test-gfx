3e1f6dec3ac6bf67aa5096927f0dd421e08d2c31
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2020-12-08 22:41:40 +0800

    taskflow

--- tests/StressTest.cc
+++ tests/StressTest.cc
@@ -295,23 +295,6 @@ void StressTest::createPipeline() {
     _pipelineState = _device->createPipelineState(pipelineInfo);
 }
 
-void HSV2RGB(const float h, const float s, const float v, float &r, float &g, float &b) {
-    int   hi = (int)(h / 60.0f) % 6;
-    float f  = (h / 60.0f) - hi;
-    float p  = v * (1.0f - s);
-    float q  = v * (1.0f - s * f);
-    float t  = v * (1.0f - s * (1.0f - f));
-
-    switch(hi) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-}
-
 void StressTest::tick()
 {
     lookupTime();
