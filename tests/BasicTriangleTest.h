0ccd3241a99973deace387549ea01e9922b803b6
Yun Hsiao Wu <yunhsiaow@gmail.com>
2022-01-17 15:33:02 +0800

    occlusion query test

--- tests/BasicTriangleTest.h
+++ tests/BasicTriangleTest.h
@@ -24,17 +24,18 @@ private:
     void createPipeline();
     void createInputAssembler();
 
-    gfx::Shader *             _shader              = nullptr;
-    gfx::Buffer *             _vertexBuffer        = nullptr;
-    gfx::Buffer *             _uniformBuffer       = nullptr;
-    gfx::Buffer *             _uniformBufferMVP    = nullptr;
-    gfx::DescriptorSet *      _descriptorSet       = nullptr;
-    gfx::DescriptorSetLayout *_descriptorSetLayout = nullptr;
-    gfx::PipelineLayout *     _pipelineLayout      = nullptr;
-    gfx::PipelineState *      _pipelineState       = nullptr;
-    gfx::InputAssembler *     _inputAssembler      = nullptr;
-    gfx::Buffer *             _indirectBuffer      = nullptr;
-    gfx::Buffer *             _indexBuffer         = nullptr;
+    gfx::Shader *             _shader                 = nullptr;
+    gfx::Buffer *             _vertexBuffer           = nullptr;
+    gfx::Buffer *             _uniformBuffer          = nullptr;
+    gfx::Buffer *             _uniformBufferMVP       = nullptr;
+    gfx::DescriptorSet *      _descriptorSet          = nullptr;
+    gfx::DescriptorSetLayout *_descriptorSetLayout    = nullptr;
+    gfx::PipelineLayout *     _pipelineLayout         = nullptr;
+    gfx::PipelineState *      _pipelineState          = nullptr;
+    gfx::PipelineState *      _invisiblePipelineState = nullptr;
+    gfx::InputAssembler *     _inputAssembler         = nullptr;
+    gfx::Buffer *             _indirectBuffer         = nullptr;
+    gfx::Buffer *             _indexBuffer            = nullptr;
 };
 
 } // namespace cc
